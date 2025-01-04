c8a2204ecba982d8d91d4418d8029068fd216706
learnershakil <learnershakil@gmail.com>
2025-01-04 15:02:15 +0530

    feat: husky setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,54 @@
+import { auth } from "@/auth"
+import SearchForm from "@/components/SearchForm"
+import StartupCard from "@/components/StartupCard"
+import { sanityFetch } from "@/sanity/lib/live"
+import { STARTUPS_QUERY } from "@/sanity/lib/queries"
+
+const page = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>
+}) => {
+  const query = (await searchParams).query
+  const params = { search: query || null }
+
+  const session = await auth()
+
+  console.log(session?.id)
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
+
+  // console.log(JSON.stringify(posts, null, 2))
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Project, <br /> Connect with Developers
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Projects, and Collaborate with Developers to
+          bring your project to life.
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Projects"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StatupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No projects found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  )
+}
+
+export default page
---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // Match all domains
+      },
+    ],
+  },
+  experimental: {
+    ppr: "incremental",
+    after: true, // Enable the unstable_after feature
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
+}
+
+export default nextConfig
