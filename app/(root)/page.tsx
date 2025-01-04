830b0e65da03be47528068e1d0b05887390b42c5
learnershakil <learnershakil@gmail.com>
2025-01-04 15:50:52 +0530

    wip: hero section done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,18 +24,18 @@ const page = async ({
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch Your Project, <br /> Connect with Developers
+          Class Notes, Project  <br /> Doc&apos;s and More...
         </h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Projects, and Collaborate with Developers to
-          bring your project to life.
+          Search for projects, notes, and more. Find what you need to get
+          started. Explore projects and notes from Learner&apos;s Arc.
         </p>
         <SearchForm query={query} />
       </section>
 
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Projects"}
+          {query ? `Search results for "${query}"` : "All Projects & Notes"}
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
@@ -43,7 +43,7 @@ const page = async ({
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
-            <p className="no-results">No projects found</p>
+            <p className="no-results">No projects/Notes found</p>
           )}
         </ul>
       </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,10 +11,10 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
+          <Image src="/learnerslab.png" alt="logo" width={144} height={30} />
         </Link>
 
-        <div className="flex items-center gap-5 text-black">
+        {/* <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
@@ -56,7 +56,7 @@ const Navbar = async () => {
               <button type="submit">Login</button>
             </form>
           )}
-        </div>
+        </div> */}
       </nav>
     </header>
   )
